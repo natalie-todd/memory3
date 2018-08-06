6552d6e987dcd114fd6496fc8c12379cf4265809
Natalie <natalie.todd15@gmail.com>
2018-08-05 18:52:44 -0600

    queries built, 1 not passing, added  cors

--- queries.js
+++ queries.js
@@ -8,9 +8,12 @@ module.exports = {
         return database('game').select().where('id', id).first();
     },
     create(game){
+        return database('game').insert(game).returning('*').then(record => record[0]);
     },
     update(id, game){
+        return database('game').update(game).where('id', id).returning('*').then(record => record[0]);
     },
     delete(id){
+        return database('game').delete().where('id', id);
     }
 };
